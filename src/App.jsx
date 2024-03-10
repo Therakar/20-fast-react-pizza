c547f2cae92b0050e316107587a3767a9123a29b
Therakar <matteo.pau200@gmail.com>
2024-03-10 15:53:45 +0100

    router creation using createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+//React Router imports
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+//Components imports
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+
+//I'll define myroutes inisde this creteBrowserRouter function
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
 function App() {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
