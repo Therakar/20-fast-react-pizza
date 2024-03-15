bcad80ca70ab3bcb1b97168f519725078dbb4f80
Therakar <matteo.pau200@gmail.com>
2024-03-15 17:38:35 +0100

    spacing and borders with tailwind

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-    return <div className="text-sm font-semibold">Matteo</div>
+    return <div className="text-sm font-semibold">Matteo</div>;
 }
 
-export default Username
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom'
-import SearchOrder from '../features/order/SearchOrder'
-import Username from '../features/user/Username'
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
     return (
-        <header className="bg-yellow-500 uppercase">
+        <header className="border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase">
             {/* use tracking-['value in px or rem'] if you want to personalize the result more*/}
             <Link to="/" className="tracking-widest">
                 Fast React Pizza Co.
@@ -14,7 +14,7 @@ function Header() {
 
             <Username />
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
