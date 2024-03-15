53fcf029694f1e509ef8bb9c5f44d58f195771b5
Therakar <matteo.pau200@gmail.com>
2024-03-15 16:10:45 +0100

    tailwind and prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,48 @@
 //React Router imports
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 //Components imports
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import Order, { loader as orderLoader } from "./features/order/Order";
+import Home from './ui/Home'
+import Menu, { loader as menuLoader } from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import Order, { loader as orderLoader } from './features/order/Order'
 import CreateOrder, {
-  action as createOrderAction,
-} from "./features/order/CreateOrder";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
+    action as createOrderAction,
+} from './features/order/CreateOrder'
+import AppLayout from './ui/AppLayout'
+import Error from './ui/Error'
 
 //I'll define myroutes inisde this creteBrowserRouter function
 const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      { path: "/", element: <Home /> },
-      {
-        path: "/menu",
-        element: <Menu />,
-        loader: menuLoader,
+    {
+        element: <AppLayout />,
         errorElement: <Error />,
-      },
-      { path: "/cart", element: <Cart /> },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-        action: createOrderAction,
-      },
-      {
-        path: "/order/:orderId",
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-      },
-    ],
-  },
-]);
+        children: [
+            { path: '/', element: <Home /> },
+            {
+                path: '/menu',
+                element: <Menu />,
+                loader: menuLoader,
+                errorElement: <Error />,
+            },
+            { path: '/cart', element: <Cart /> },
+            {
+                path: '/order/new',
+                element: <CreateOrder />,
+                action: createOrderAction,
+            },
+            {
+                path: '/order/:orderId',
+                element: <Order />,
+                loader: orderLoader,
+                errorElement: <Error />,
+            },
+        ],
+    },
+])
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+    return <RouterProvider router={router}></RouterProvider>
 }
 
-export default App;
+export default App
