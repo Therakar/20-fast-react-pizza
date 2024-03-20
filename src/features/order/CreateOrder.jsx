d59467aa83b385a3a8532baf9176e418f6d9e065
Therakar <matteo.pau200@gmail.com>
2024-03-20 18:18:54 +0100

    Use the username global state wherever I need it

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -42,6 +43,7 @@ const fakeCart = [
 function CreateOrder() {
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
+    const username = useSelector((state) => state.user.username);
 
     const formErrors = useActionData();
 
@@ -60,6 +62,7 @@ function CreateOrder() {
                         className="input grow"
                         type="text"
                         name="customer"
+                        defaultValue={username}
                         required
                     />
                 </div>
